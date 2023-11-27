7e51d142fc00468872bb43f1268ed8d996b82720
Marcin Macieszczuk <m.macieszczuk@gmail.com>
2023-11-27 16:28:22 +0100

    created SubtitleFileParser class

--- subtitle_shift.py
+++ subtitle_shift.py
@@ -7,52 +7,57 @@ def main():
     file_content = read_file()
     parse_file(file_content)
     
-    for sub in Subtitle.subtitles[:5]:
+    for sub in SubtitleBlock.subtitle_blocks[:5]:
         print(sub.str_full())
     
-def read_file(to_list=False):
-    with open('subtitles.srt', 'r') as file:
-        if to_list:
-            return file.readlines()
-        else:
-            return file.read()
 
-def parse_file(file_content: str):
-    if not file_content:
-        return
-    elif type(file_content) != str:
-        raise TypeError('Incorrect argument type. Str expected.')
-    
-    Subtitle.subtitles.clear()
+class SubtitleFileParser:
+    def __init__(self) -> None:
+        pass
 
-    pattern = r'(\d+)\n(\d\d.*\d\d\d) --> (\d\d.*\d\d\d)\n(?:(?:\n\n)|((?:.+\n)+))'
-    subtitle_blocks = re.findall(
-        pattern,
-        file_content
-    )
+    def read_file(to_list=False):
+        with open('subtitles.srt', 'r') as file:
+            if to_list:
+                return file.readlines()
+            else:
+                return file.read()
 
-    for block in subtitle_blocks:
-        try:
-            Subtitle.subtitles.append(
-                Subtitle(
-                    order=int(block[0]),
-                    start_time=datetime.strptime(block[1], SUBTITLE_TIME_FORMAT),
-                    end_time=datetime.strptime(block[2], SUBTITLE_TIME_FORMAT),
-                    text=block[3]
+    def parse_file(file_content: str):
+        if not file_content:
+            return
+        elif type(file_content) != str:
+            raise TypeError('Incorrect argument type. Str expected.')
+        
+        SubtitleBlock.subtitle_blocks.clear()
+
+        pattern = r'(\d+)\n(\d\d.*\d\d\d) --> (\d\d.*\d\d\d)\n(?:(?:\n\n)|((?:.+\n)+))'
+        subtitle_blocks = re.findall(
+            pattern,
+            file_content
+        )
+
+        for block in subtitle_blocks:
+            try:
+                SubtitleBlock.subtitle_blocks.append(
+                    SubtitleBlock(
+                        order=int(block[0]),
+                        start_time=datetime.strptime(block[1], SUBTITLE_TIME_FORMAT),
+                        end_time=datetime.strptime(block[2], SUBTITLE_TIME_FORMAT),
+                        text=block[3]
+                    )
                 )
-            )
-        except ValueError as ve:
-            print('Error in block: ' + str(block))
-            print(ve)
-            continue
-        except Exception as exc:
-            print('Error in block: ' + str(block))
-            print(exc)
-            continue
+            except ValueError as ve:
+                print('Error in block: ' + str(block))
+                print(ve)
+                continue
+            except Exception as exc:
+                print('Error in block: ' + str(block))
+                print(exc)
+                continue
 
 
-class Subtitle:
-    subtitles = []
+class SubtitleBlock:
+    subtitle_blocks = []
 
     def __init__(self):
         pass
